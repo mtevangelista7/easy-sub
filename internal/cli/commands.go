7c2933e3dddd8b4fcb4548a162a21ac87545813b
Matheus Evangelista Dos Santos <matheus.evangelista7@protonmail.com>
2024-07-15 21:21:56 -0300

    feat: add some code to complete the user choose movie part

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -1,19 +1,33 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
 
 var findCmd = &cobra.Command{
 	Use:   "find",
 	Short: "find all movies files",
 	Run: func(cmd *cobra.Command, args []string) {
-
 		var path string
-
-		if len(args) != 0 {
-			path = args[0]
+		if len(args) == 0 {
+			return
 		}
 
-		PerformSearch(path)
+		// pega o path
+		path = args[0]
+
+		// busca a lista de arquivos de vídeo existentes na pasta
+		// e solicita para o usuário qual ele deseja
+		nameFile := AskForFileName(PerformSearch(path))
+
+		if nameFile == "" {
+			return
+		}
+
+		fmt.Printf("Nome do filme: %s\n", nameFile)
+
+		// mandar para a api e começar a buscar pela legenda
 	},
 }
 
--- internal/cli/operations.go
+++ internal/cli/operations.go
@@ -1,12 +1,46 @@
 package cli
 
-import "easysub/internal/io"
+import (
+	"bufio"
+	"easysub/internal/io"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-func PerformSearch(path string) {
+func PerformSearch(path string) []string {
 	// pega uma lista com os arquivos de áudio
-	filesNames := io.SearchAllFileName(path)
+	fileNames := io.SearchAllFileName(path)
+	return fileNames
+}
 
-	for _, name := range filesNames {
-		print("%s", name)
+func AskForFileName(fileNames []string) string {
+	fmt.Println("Please, choose one movie")
+
+	// printa cada uma das opções para o usuário
+	for index, fileName := range fileNames {
+		fmt.Printf("[%d] - %s\n", index, fileName)
 	}
+
+	// aguarda a reposta
+	reader := bufio.NewReader(os.Stdin)
+
+	// realiza a leitura
+	userInput, _ := reader.ReadString('\n')
+
+	// remove o \n (todo não peguei muito o do readstring)
+	userInput = userInput[:len(userInput)-1]
+
+	// converte a opção selecionada pela o usuario
+	userChoose, err := strconv.Atoi(userInput)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Pega a opção selecionada pelo usuario
+	movieName := fileNames[userChoose]
+
+	// retorna o nome do filme
+	return movieName
 }
